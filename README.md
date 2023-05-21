7f83be9c01323f2406474ec915fda57917145710
Kwvus <iklvzss@gmail.com>
2023-05-21 21:07:49 +0200

    Init

---
+++ README.md
@@ -0,0 +1,56 @@
+
+# RBLX-item-scraper
+
+This was designed to check the price of a roblox **limited** item every few minutes with a custom delay option available. It will notify you by sending a message to your discord server using a webhook.
+
+
+
+## Author
+
+- [@silentswrd](https://github.com/silentswrd)
+
+
+## Python3 & Chrome required
+
+[Python3](https://www.python.org/downloads/) is required to run. If you use Python2 or don't have Python installed please upgrade to avoid conflict. The code utilises [Chrome](https://www.google.com/chrome/) to scrape information correctly, make sure it is installed to the default path.
+    
+## Usage / Instructions
+
+#### IMPORTANT: Before you start
+
+Note that you should change the settings to be towards your liking in `settings.json` and to change `webhook_url` in `settings.json` to the webhook url from your Discord server's webhook. Find out [how to set up a webhook](https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks). In this file you can also change the URL to the item you wish to price scrape. Do not change any files other than `settings.json`
+
+#### MacOS / Linux:
+1. Open `terminal` by searching for it or opening it via <kbd>⌘</kbd> + <kbd>SPACE</kbd> and typing `terminal`
+2. Run `cd [PATH TO THE FOLDER]`
+* Choose View > Show Path Bar, or press the <kbd>⌃</kbd> key to show the path bar momentarily.
+3. Run `pip3 install -r requirements.txt`
+4. Run `python3 core.py`
+
+#### Windows:
+1. Open `cmd` by searching for it or opening it via <kbd>WIN</kbd> + <kbd>R</kbd> and typing `cmd`
+2. Run `cd [PATH TO THE FOLDER]`
+3. Run `pip3 install -r requirements.txt`
+4. Run `python3 core.py`
+## Contributing
+
+Contributions are always welcome!
+
+Visit [@silentswrd on Ko-fi](https://ko-fi.com/silentswrd)
+
+Tips make it easier for me to work on current and upcoming projects and help me with the cost to maintain them.
+## Lisence
+
+Attribution-NonCommercial 2023 silencedswrd
+
+This work is licensed under the Attribution-NonCommercial 3.0 Unported (CC BY-NC 3.0) License. To view a copy of this license, visit https://creativecommons.org/licenses/by-nc/3.0/.
+
+You are free to:
+
+- Share: Copy and redistribute the material in any medium or format.
+- Adapt: Remix, transform, and build upon the material.
+
+Under the following terms:
+
+- Attribution: You must give appropriate credit, provide a link to the license, and indicate if changes were made. You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
+- NonCommercial: You may not use the material for commercial purposes.
---
+++ core.py
@@ -0,0 +1,76 @@
+import time, datetime, sys, os, json
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
+import requests
+
+def log_price(price):
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open(os.path.join(sys.path[0], settings['log_file']), 'a') as f:
+        f.write(f'{timestamp}: R$ {price}\n')
+
+def send_notification(price):
+    if price <= settings['min_price']:
+        message = f"<@{settings['discord_user_id']}> **SUCCESS:** Price reached __**R$ {price}**__.\n{settings['target']}"
+        data = {
+            "content": message
+        }
+        response = requests.post(settings['webhook_url'], json=data)
+        if response.status_code == 204:
+            print("\u001b[32mNotification sent to Discord webhook successfully.\u001b[0m")
+            sys.stdout.flush()
+        return True
+
+def print_error_message(message):
+    print("\033[31m{}\033[0m".format(message))
+    sys.stdout.flush()
+
+# Load settings from JSON file
+with open(os.path.join(sys.path[0], 'settings.json')) as f:
+    settings = json.load(f)
+
+retry_attempts = 0
+while retry_attempts < settings['max_retry_attempts']:
+    success = False # The default state of success should be False, because we haven't succeeded yet.
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get(settings['target'])
+    time.sleep(settings['retry_delay'])
+
+    try:
+        soup = BeautifulSoup(driver.page_source, "lxml")
+        elements = soup.find_all("span", class_="text-robux-lg")
+        desired_element = None
+        for element in elements:
+            if "disabled" not in element["class"]:
+                desired_element = element
+                break
+
+        if desired_element:
+            price = int(desired_element.text)
+            driver.quit()
+            print(f"Current price: R$ {price}")
+            log_price(price)
+            if send_notification(price):
+                driver.quit()
+                sys.exit(0)
+            
+            # Reset retry attempts to 0
+            retry_attempts = 0
+            success = True
+
+            # Delay
+            time.sleep(20)
+    except NoSuchElementException:
+        pass
+
+    if retry_attempts < settings['max_retry_attempts'] and success == False:
+        driver.quit()
+        retry_attempts += 1
+        print(f"Page got stuck loading. Retry attempt {retry_attempts}...")
+        time.sleep(2)  # Wait for 2 seconds before the next attempt
+
+error_message = f"Desired element not found after {settings['max_retry_attempts']} retry attempts of {settings['retry_delay']} seconds each."
+print_error_message(error_message)
+log_price(error_message)
